d9dfcdba0aca89ec0a4979afdc9dbffd53bf1a93
Tshenolo-C <ST10469987@myemeris.edu.za>
2026-03-27 15:48:59 +0200

    Add project files.

---
+++ Prog_6221_ST10469987_POE_Part1/ConsoleUI.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Prog_6221_ST10469987_POE_Part1
+{
+    internal class ConsoleUI
+    {
+        public static void ConColour(string text, ConsoleColor colour, bool newLine = true)
+        {
+            Console.ForegroundColor = colour;
+            if (newLine)
+            {
+                Console.WriteLine(text);
+            }
+            else
+            {
+                Console.WriteLine(text);
+                Console.ResetColor();
+            }
+
+        }
+
+        public static void Writing(string text, ConsoleColor colour = ConsoleColor.Gray, int Delayms = 16)
+        {
+            Console.ForegroundColor = colour;
+            foreach (char c in text)
+            {
+                Console.Write(c);
+                Thread.Sleep(Delayms);
+            }
+            Console.ResetColor();
+            Console.WriteLine();
+        }
+
+        public static void Divider(char symbol = '-', int width = 60, ConsoleColor colour = ConsoleColor.DarkGreen)
+        {
+            ConColour(new string(symbol, width), colour);
+        }
+
+        public static void DisplayLogo()
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine(@"
+                             .-------.
+                            /         \
+                           |           |
+                           |           |
+                            \         /
+                    .-------'-........'------.
+                    |  .-------------------. |
+                    | | .----------------. | |
+                    | | |                | | |
+                    | | |       ()       | | |
+                    | | |       ||       | | |
+                    | | |       ||       | | |
+                    | | '----------------' | |
+                    | '-------------------'  |
+                    '------------------------'  
+            ");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine(@"
+               +-------------------------------------------------+
+               |       CYBERSECURITY AWARENESS BOT               |
+               |         Your Digital Safety Guide               |
+               +-------------------------------------------------+
+            ");
+            Console.ResetColor();
+        }
+
+        public static void Header(string title) 
+        {
+            Console.WriteLine();
+            Divider('-', 60, ConsoleColor.DarkGreen);
+            ConColour(" " + title, ConsoleColor.Green);
+            Divider('-', 60, ConsoleColor.DarkGreen);
+        }
+
+
+        public static void Botname()
+        {
+            ConColour("Cyber Bot >>", ConsoleColor.DarkGreen, newLine: false);
+
+        }
+
+        public static void Userprompt(string userName)
+        {
+            ConColour("\n" + userName + ">>", ConsoleColor.Red, newLine: false);
+        }
+
+    }
+}
---
+++ Prog_6221_ST10469987_POE_Part1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Prog_6221_ST10469987_POE_Part1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Chatbot bot= new Chatbot
+            bot.Start();
+        }
+    
+    }
+}
